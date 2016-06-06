d66dcb5cd4973e9a8fb8c490b865e5479cbb55bb
lucas <lucas@axlot.com>
2016-06-06 18:01:33 -0300

    Attend events

--- brain/events.js
+++ brain/events.js
@@ -185,9 +185,8 @@ module.exports.init = function(controller) {
                         bot.startConversation(message, function(err, convo) {
                             bot.api.users.info({user: message.user}, function(err, user) {
                                 convo.say('Hey, ' + user.user.real_name + ' there is no event with that ID!');
-                                convo.next();
                             });
-                            convo.next();
+                            convo.stop();
                         });
                     }
                 });
