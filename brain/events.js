7f30579cd43bb7aea946190f746a1c7f6ec026d7
lucas <lucas@axlot.com>
2016-06-06 18:07:03 -0300

    Attend events

--- brain/events.js
+++ brain/events.js
@@ -179,14 +179,15 @@ module.exports.init = function(controller) {
                                 }
                                 convo.next();
                             }
-                            convo.next();
+                            //convo.next();
                         });
                     } else {
                         bot.startConversation(message, function(err, convo) {
                             bot.api.users.info({user: message.user}, function(err, user) {
                                 convo.say('Hey, ' + user.user.real_name + ' there is no event with that ID!');
-                                convo.next();
+                                //convo.next();
                             });
+                            convo.next();
                         });
                     }
                 });
