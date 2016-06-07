1362d00179fcf98144f57151481ecd1bc9cf5a4c
lucas <lucas@axlot.com>
2016-06-07 15:10:38 -0300

    Maybe And Not (Attend)

--- brain/events.js
+++ brain/events.js
@@ -380,7 +380,7 @@ module.exports.init = function(controller) {
                         }
                         maybe[user] = true;
                         //Save Attend
-                        controller.storage.rsvp.save({id: 'event_' + eventId, maybe:maybe}, function(err) {});
+                        controller.storage.rsvp.maybe.save({id: 'event_' + eventId, maybe:maybe}, function(err) {});
                     });
                 } else {
                     bot.startConversation(message, function(err, convo) {
