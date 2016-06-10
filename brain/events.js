482c5213720d9d932264d2c7046c2ca67b71b655
lucas <lucas@axlot.com>
2016-06-10 15:44:09 -0300

    Validate User To Edit An Event Function

--- brain/events.js
+++ brain/events.js
@@ -42,6 +42,11 @@ module.exports.init = function(controller) {
         //Check Team ID
         controller.storage.events.get(eventId, function(err, event_data){
             bot.reply(message, 'La ID del solicitante es: ' + message.user + ' y la ID del creador es: ' + event_data.event_data.user_id);
+            if(message.user == event_data.event_data.user_id) {
+                return true;
+            } else {
+                return false;
+            }
         });
     };
     //Year Of Event
@@ -579,13 +584,12 @@ module.exports.init = function(controller) {
         //Get Event ID
         var eventId = message.match[1];
         //Call To Validate User Function
-        validateUser(bot,message,eventId);
-        //if(validateUser(message,eventId)) {
-        //    //Start Conversation
-        //    createEvent(bot, message, eventId);
-        //} else {
-        //    bot.reply(message, 'This is not the event you\'re looking for...');
-        //}
+        if(validateUser(bot,message,eventId)) {
+            //Start Conversation
+            createEvent(bot, message, eventId);
+        } else {
+            bot.reply(message, 'This is not the event you\'re looking for...');
+        }
     });
     //Conversation Controller "ATTEND EVENT"
     controller.hears('attend (.*)',['direct_message','direct_mention'],function(bot,message) {
