1c33f35b97c5d6b433751627299f2ffd653cc763
lucas <lucas@axlot.com>
2016-06-13 16:51:04 -0300

    User's Reaction To Event

--- brain/events.js
+++ brain/events.js
@@ -859,9 +859,6 @@ module.exports.init = function(controller) {
                     console.log('EL TS DE ' + all_events_data[i].event_data.title + ' ES ' + all_events_data[i].event_data.mTimeStamp);
                     console.log('EL TS DEL EVENTO REQUERIDO ES: ' + message.item.ts);
                     console.log('EL EVENT TS ES: ' + message.event_ts);
-                    if(all_events_data[i].event_data.mTimeStamp == message.item.ts) {
-                        console.log('======================EVENTO ENCONTRADO=======================');
-                    }
                 }
             });
         //RSVP Maybe
