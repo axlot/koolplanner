75778fb728caf8fc94dc8a404a6bff4612447bde
lucas <lucas@axlot.com>
2016-06-13 15:44:44 -0300

    User's Reaction To Event

--- brain/events.js
+++ brain/events.js
@@ -840,6 +840,6 @@ module.exports.init = function(controller) {
     controller.on('reaction_added', function(bot, message) {
         //Get Reaction
         console.log('============================ACA EMPIEZA LA RESPUESTA==================================');
-        console.dir(message);
+        console.dir(message.item);
     });
 };
