f3b8a537a1d0f5dc01bb87fdf8eb18728a5b5bbe
Lautaro Rosales <lautaro@axlot.com>
2016-06-14 16:47:46 -0300

    fix bot definition

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ beepboop.on('add_resource', function (message) {
     var bot = beepboop.workers[id];
 
       //Cron Task
-      cron.schedule('4 * * * * *', function(bot){
+      cron.schedule('4 * * * * *', function(){
           events.notify(bot);
           console.log('===========================EL CRON ESTA FUNCIONANDO=========================');
       });
