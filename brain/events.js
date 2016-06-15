c95d815cb6435fa092209751b1bf8995da0280c1
lucas <lucas@axlot.com>
2016-06-15 17:42:24 -0300

    Upcoming Events

--- brain/events.js
+++ brain/events.js
@@ -819,7 +819,7 @@ module.exports.notify = function(controller, bot) {
             //Iterate Over Attenddes Obj And Get User's Names
             for(var userID in attendees){
                 bot.startPrivateConversation({user: userID}, function(err, convo){
-                    bot.botkit.bot.say('Hey ' + user.user.name + '!\n' + customMessage);
+                    //bot.botkit.bot.say('Hey ' + user.user.name + '!\n' + customMessage);
                     //bot.api.users.info({user: convo.source_message.user}, function(err, user) {
                     //    conversation.say('Hey ' + user.user.name + '!\n' + customMessage);
                     //});
