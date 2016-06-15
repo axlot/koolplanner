d19af94fa051c17f5898c7d063953edf845e6725
lucas <lucas@axlot.com>
2016-06-15 16:06:30 -0300

    Upcoming Events

--- brain/events.js
+++ brain/events.js
@@ -807,8 +807,7 @@ module.exports.notify = function(controller, bot) {
     function alertAttendeesToEvent(bot, customMessage, eventId, controller) {
         console.log('ENTRA A LA FUNCION ALERTATENDEES');
         controller.storage.rsvp.all(function(err, all_attend_data) {
-            console.log('================ATTENDESSSSSS==================');
-            console.dir(all_attend_data);
+            console.log('================ATTENDES==================');
             console.log('LENGHT OF ATTEND DATA: ' + all_attend_data.attend.length);
             //var length = all_attend_data.length,
             //    attendees;
