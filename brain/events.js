2c9f276933f51e01affb1875d71b64d76b0a91ca
lucas <lucas@axlot.com>
2016-06-23 16:50:56 -0300

    Broadcast Messages On Team

--- brain/events.js
+++ brain/events.js
@@ -756,8 +756,8 @@ module.exports.init = function(controller) {
         });
     });
     //Event "JOIN"
-    controller.on('bot_added',function(bot,message) {
-        console.log('////////////////////BOT ADDED EVENT///////////////////////////');
+    controller.on('bot_channel_join',function(bot,message) {
+
         //Onboarding Message Here
         bot.api.chat.postMessage({
             "text": "Hey there!:wave: I’m your KoolPlanner, your event planning assistant. I’m here to help you plan events without hassle. :spiral_calendar_pad:",
