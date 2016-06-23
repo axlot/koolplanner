bd7f9fa13c3004a0d11b1a655672572dd3a98b46
lucas <lucas@axlot.com>
2016-06-23 16:01:09 -0300

    Broadcast Messages On Team

--- brain/events.js
+++ brain/events.js
@@ -758,9 +758,9 @@ module.exports.init = function(controller) {
     //Temporal Controller "TEAM DATA"
     controller.hears('team data',['direct_message','direct_mention', 'mention'],function(bot,message) {
         console.log('//////////////TEAM ID///////////////////');
-        console.dir(message);
+        console.dir(message.team);
         //Save The Channel Where The Bot Was Added
-        controller.storage.teams.get(message.team_id, function(err, team_data){
+        controller.storage.teams.get(message.team, function(err, team_data){
             console.dir(team_data);
         });
     });
